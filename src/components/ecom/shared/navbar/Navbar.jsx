3a4170c9e9cca851e94a70fe45a1b731f65b07f4
Mohankumar <mohan2609kumar@gmail.com>
2023-12-12 14:22:32 +0530

    toggle menu added

--- src/components/ecom/shared/navbar/Navbar.jsx
+++ src/components/ecom/shared/navbar/Navbar.jsx
@@ -1,6 +1,10 @@
 import "./navbar.scss";
 
+import ToggleMenuBtn from "./toggleMenuBtn/ToggleMenuBtn";
+
+
 const Navbar = () => {
+
     return (
         <div className="navbar">
             <div className="logo">
@@ -8,18 +12,7 @@ const Navbar = () => {
                 <span>lamadmin</span>
             </div>
             <div className="icons">
-                <div className="notification">
-                    <img src="/notifications.svg" alt="" />
-                    <span>1</span>
-                </div>
-                <div className="user">
-                    <img
-                        src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
-                        alt=""
-                    />
-                    <span>Jane</span>
-                </div>
-                <img src="/settings.svg" alt="" className="icon" />
+                <ToggleMenuBtn />
             </div>
         </div>
     );
